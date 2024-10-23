39b2ab47a97a2940ec1ca7ca729ad525c93f7103
fernanda1417 <135229119+fernanda1417@users.noreply.github.com>
2024-10-23 07:52:52 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,2 +1,9 @@
-const url = "";
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json";
 
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    cont dados = await res.json()
+    console.log(dados);
+}
+
+visualizarInformacoesGlobais();
